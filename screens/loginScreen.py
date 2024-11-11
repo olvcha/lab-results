a2857e590b5974633674389c146d1f6c59a9794d
Aleksandra Boruczkowska <ola@MacBook-Air-Aleksandra.local>
2024-11-11 13:30:16 +0100

    Created widgets in registration screen

--- screens/loginScreen.py
+++ screens/loginScreen.py
@@ -8,8 +8,13 @@ Builder.load_file(os.path.join(os.path.dirname(__file__), 'login_screen.kv'))
 
 
 class LoginScreen(MDScreen):
+    '''Handles login screen actions'''
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
 
     def switch_to_new_result_screen(self):
         self.manager.current = 'new_result'
+
+    def switch_to_registration_screen(self):
+        '''Switches to the registration screen'''
+        self.manager.current = 'registration'
